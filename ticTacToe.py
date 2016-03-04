8c99142e1b5852f4a97f62a48fe2d0f33449c38a
Nathan Spencer <nspencer1@mail.valenciacollege.edu>
2016-03-04 15:53:25 -0500

    Building the ticTacToe program
    v0.5

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,23 @@
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R':
+' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
 def printBoard(board):
+       print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+       print('-+-+-')
+       print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+       print('-+-+-')
+       print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+turn = 'X'
+for i in range(9):
+       printBoard(theBoard)
+       print('Turn for ' + turn +'. Move on which space?')
+       move = input()
+       theBoard[move] = turn
+       if turn == 'X':
+              turn = 'O'
+       else:
+              turn = "X"
+printBoard(theBoard)
     # TO DO #################################################################
     # Write code in this function that prints the game board.               #
     # The code in this function should only print, the user should NOT      #
@@ -9,6 +28,8 @@ def printBoard(board):
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
+
+    
     
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
