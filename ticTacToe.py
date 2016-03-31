24757f631eb27ec83cf3f24028b94652696d9be5
Nathan Spencer <nspencer1@mail.valenciacollege.edu>
2016-03-31 12:56:08 -0400

    Tictactoe
    Return False

--- ticTacToe.py
+++ ticTacToe.py
@@ -17,22 +17,24 @@ def printBoard(board):
 
 def checkWinner(board, player):
        print('Checking if ' + player + ' is a winner...')
-       if player == 'top-L' and player == 'top-M' and player == 'top-R': #horizontal
-              return True
+       if player == 'top-L' and player == 'top-M' and player == 'top-R':#horizontal
+                  return True
        elif player == 'mid-L' and player == 'mid-M' and player == 'mid-R':
-              return True
+                  return True
        elif player == 'low-L' and player == 'low-M' and player == 'low-R':
-              return True
+                  return True
        elif player == 'top-R' and player == 'mid-R' and player == 'low-R': #vertical 
-              return True
+                  return True
        elif player == 'top-M' and player == 'mid-M' and player == 'low-M':
-              return True
+                  return True
        elif player == 'top-L' and player == 'mid-L' and player == 'low-L':
-              return True
+                  return True
        elif player == 'top-L' and player == 'mid-M' and player == 'low-R': #diagonal
-              return True
+                  return True
        elif player == 'top-R' and player == 'mid-M' and player == 'low-L':
-               return True
+                  return True
+       else:
+              return False
        
 
 
